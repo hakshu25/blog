af9137c3806823c50fb3b727802b7e62fb695c76
hakshu25 <hakshu25@gmail.com>
2024-07-14 18:09:30 +0900

    chore: shadcn/vue を設定した

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+
   runtimeConfig: {
     cmsApiDomain: process.env.CMS_API_DOMAIN,
     cmsApiKey: process.env.CMS_API_KEY
+  },
+
+  ssr: true,
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxt/image"],
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui'
   }
 })
